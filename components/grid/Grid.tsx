9e3594611dc55b9e216a80b8e6af2e8567c53e6a
xand974 <xand974@gmail.com>
2022-07-25 22:40:23 +0200

    handle animation when exit

--- components/grid/Grid.tsx
+++ components/grid/Grid.tsx
@@ -15,46 +15,46 @@ export default function Grid() {
 
   return (
     <AnimatePresence exitBeforeEnter>
-      <>
-        {expandGrid ? (
-          <motion.div
-            className="grid__align"
-            variants={gridVariant}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-          >
-            {GRID_IMAGES.map((item, index) =>
-              item.map((image, id) => (
-                <div className="grid__align__content" key={id}>
-                  <motion.img
-                    variants={imageGridVariant}
-                    initial="initial"
-                    animate="animate"
-                    className="grid__align__content__img"
-                    layoutId={`grid-img-${id}x${index}`}
-                    src={image}
-                  />
-                </div>
-              ))
-            )}
-          </motion.div>
-        ) : (
-          <motion.div
-            variants={screenGridVariant}
-            animate="animate"
-            initial="initial"
-            exit="exit"
-            className="grid__expand"
-          >
-            {GRID_IMAGES.map((item, index) =>
-              item.map((image, id) => (
-                <GridImage image={image} col={index} currentKey={id} key={id} />
-              ))
-            )}
-          </motion.div>
-        )}
-      </>
+      {expandGrid ? (
+        <motion.div
+          key="motion-grid-key"
+          className="grid__align"
+          variants={gridVariant}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          {GRID_IMAGES.map((item, index) =>
+            item.map((image, id) => (
+              <div className="grid__align__content" key={id}>
+                <motion.img
+                  variants={imageGridVariant}
+                  initial="initial"
+                  animate="animate"
+                  className="grid__align__content__img"
+                  layoutId={`grid-img-${id}x${index}`}
+                  src={image}
+                />
+              </div>
+            ))
+          )}
+        </motion.div>
+      ) : (
+        <motion.div
+          key="motion-grid-key"
+          variants={screenGridVariant}
+          animate="animate"
+          initial="initial"
+          exit="exit"
+          className="grid__expand"
+        >
+          {GRID_IMAGES.map((item, index) =>
+            item.map((image, id) => (
+              <GridImage image={image} col={index} currentKey={id} key={id} />
+            ))
+          )}
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
